d80506669c51685b92353412b89281d790a39e21
hmmmhmmm <79254292+hmmmhmmm@users.noreply.github.com>
2022-04-28 17:55:33 +0530

    Add files via upload

---
+++ pro-1 pocketMoney(conditional proggraming).py
@@ -0,0 +1,7 @@
+pocketMoney = int( input("enter the pocket money you got : "))
+if (pocketMoney>500):
+    print("you're a rich kid!")
+elif (pocketMoney>100):
+    print("you have a good life")
+else:
+    print("I know how you feel")
